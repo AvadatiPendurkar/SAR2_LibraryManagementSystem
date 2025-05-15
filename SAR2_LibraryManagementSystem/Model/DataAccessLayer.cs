00c203127e68d167bff473cbe71d3d1563bf7a0b
AvadatiPendurkar <avadatipendurkar@gmail.com>
2025-05-15 16:53:03 +0530

    added connectionstring

--- SAR2_LibraryManagementSystem/Model/DataAccessLayer.cs
+++ SAR2_LibraryManagementSystem/Model/DataAccessLayer.cs
@@ -1,7 +1,14 @@
-﻿namespace SAR2_LibraryManagementSystem.Model
-{
-    public class DataAccessLayer
-    {
+﻿namespace SAR2_LibraryManagementSystem.Model;
+using System.Data.SqlClient;
+using System.Data;
 
+public class DataAccessLayer
+{
+    private readonly string _connectionString;
+
+    public DataAccessLayer(IConfiguration configuration)
+    {
+        _connectionString = configuration.GetConnectionString("DefaultConnection");
     }
+
 }
