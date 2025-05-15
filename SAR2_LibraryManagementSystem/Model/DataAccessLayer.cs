b78b39c924624796edb27ee371d6aa15b418d33f
AvadatiPendurkar <avadatipendurkar@gmail.com>
2025-05-15 17:21:28 +0530

    added method addUser

--- SAR2_LibraryManagementSystem/Model/DataAccessLayer.cs
+++ SAR2_LibraryManagementSystem/Model/DataAccessLayer.cs
@@ -11,4 +11,28 @@ public class DataAccessLayer
         _connectionString = configuration.GetConnectionString("DefaultConnection");
     }
 
+    public void AddUser(Users user)
+    {
+        using (var con = new SqlConnection(_connectionString))
+        {
+            con.Open();
+
+            string sqlqury = "sp_addUser";
+
+
+            using (var cmd = new SqlCommand(sqlqury, con))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@firstName", user.firstName);
+                cmd.Parameters.AddWithValue("@lastName", user.lastName);
+                cmd.Parameters.AddWithValue("@email", user.email);
+                cmd.Parameters.AddWithValue("@password", user.password);
+                cmd.Parameters.AddWithValue("@mobileNo", user.mobileNo);
+
+                int affectedrow = cmd.ExecuteNonQuery();
+
+            }
+        }
+    }
+
 }
