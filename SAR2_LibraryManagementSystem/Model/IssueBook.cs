c4a31569c192ac2cb770493d61babea8eb147a7f
charlie2307 <riteshalim237@gmail.com>
2025-05-15 16:39:56 +0530

    properties in issueBook class

--- SAR2_LibraryManagementSystem/Model/IssueBook.cs
+++ SAR2_LibraryManagementSystem/Model/IssueBook.cs
@@ -2,5 +2,13 @@
 {
     public class IssueBook
     {
+        public int issueId {  get; set; }
+        public int userId { get; set; }
+        public int bookId { get; set; }
+        public DateTime issueDate { get; set; }
+        public DateTime dueDate { get; set; }
+        public DateTime returnDate { get; set; }
+        public int bookQty { get; set; }
+        public string status { get; set; }
     }
 }
