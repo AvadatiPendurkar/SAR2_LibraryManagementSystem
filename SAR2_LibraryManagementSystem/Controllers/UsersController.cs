a85035062a3826608c7ce8700076fb5209a2ced7
charlie2307 <riteshalim237@gmail.com>
2025-05-15 17:22:33 +0530

    added httpPost to users

--- SAR2_LibraryManagementSystem/Controllers/UsersController.cs
+++ SAR2_LibraryManagementSystem/Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SAR2_LibraryManagementSystem.Model;
 
 namespace SAR2_LibraryManagementSystem.Controllers
 {
@@ -7,5 +8,18 @@ namespace SAR2_LibraryManagementSystem.Controllers
     [ApiController]
     public class UsersController : ControllerBase
     {
+        private readonly DataAccessLayer _dataAccessLayer;
+
+        public UsersController(DataAccessLayer dataAccessLayer)
+        {
+            _dataAccessLayer = dataAccessLayer;
+        }
+
+        [HttpPost]
+        public IActionResult AddUser(Users user)
+        {
+            _dataAccessLayer.AddUser(user);
+            return Ok("User added successfully");
+        }
     }
 }
