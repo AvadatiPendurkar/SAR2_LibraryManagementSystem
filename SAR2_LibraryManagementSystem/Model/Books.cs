91d2a65c479546f5de65b4021fe639093e0500f6
charlie2307 <riteshalim237@gmail.com>
2025-05-15 16:29:22 +0530

    added book class and add properties in books and users

---
+++ SAR2_LibraryManagementSystem/Model/Books.cs
@@ -0,0 +1,12 @@
+﻿namespace SAR2_LibraryManagementSystem.Model
+{
+    public class Books
+    {
+        public int bookId { get; set; }
+        public string bookName { get; set; }
+        public string authorName { get; set; }
+        public string isbn { get; set; }
+        public string genre { get; set; }
+        public int quantity { get; set; }
+    }
+}
--- SAR2_LibraryManagementSystem/Model/Users.cs
+++ SAR2_LibraryManagementSystem/Model/Users.cs
@@ -2,5 +2,11 @@
 {
     public class Users
     {
+        public int userId { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string mobileNo { get; set; }
     }
 }
