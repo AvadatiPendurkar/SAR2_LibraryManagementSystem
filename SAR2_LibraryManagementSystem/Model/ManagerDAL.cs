17cc91e4d9b7e75319fa32e021e4600c128a4d31
Srohit90 <rohitshinde43434@gmail.com>
2025-05-16 18:47:23 +0530

    update for manager

--- SAR2_LibraryManagementSystem/Model/ManagerDAL.cs
+++ SAR2_LibraryManagementSystem/Model/ManagerDAL.cs
@@ -16,11 +16,11 @@ public class ManagerDAL
     {
         using (var conn = new SqlConnection(_connectionString))
         {
-            
+
             string insertquery = "INSERT INTO Managers (mfirstName, mlastName, email, pass, mobileNo) VALUES (@mfirstName, @mlastName, @email,@mobileNo, @pass)";
             conn.Open();
             using (var cmd = new SqlCommand(insertquery, conn))
-            {                
+            {
                 cmd.Parameters.AddWithValue("@mfirstName", manager.mfirstName);
                 cmd.Parameters.AddWithValue("@mlastName", manager.mlastName);
                 cmd.Parameters.AddWithValue("@email", manager.email);
@@ -30,16 +30,68 @@ public class ManagerDAL
                 int affectedrow = cmd.ExecuteNonQuery();
 
             }
+        }
 
+    }
 
+    //update manager
+    public void UpdateManager(Managers manager)
+    {
+        using (var conn = new SqlConnection(_connectionString))
+        {
 
+            conn.Open();
+            string updateQuery = "UPDATE Managers  SET mfirstName = @mfirstName, mlastName = @mlastName, email = @email, mobileNo =@mobileNo = @pass WHERE mId = @mId";
 
+            using (var cmd = new SqlCommand(updateQuery, conn))
+            {
+                cmd.Parameters.AddWithValue("@mId", manager.mId);
+                cmd.Parameters.AddWithValue("@mfirstName", manager.mfirstName);
+                cmd.Parameters.AddWithValue("@mlastName", manager.mlastName);
+                cmd.Parameters.AddWithValue("@email", manager.email);
+                cmd.Parameters.AddWithValue("@mobileNo", manager.mobileNo);
+                cmd.Parameters.AddWithValue("@pass", manager.pass);
 
-
-
-
+            }
 
         }
     }
+
+    public List<Managers> GetAllManagers() {
+        var manager = new List<Managers>();
+        
+        using (var conn = new SqlConnection(_connectionString))
+        {
+            //  string query = "SELECT * FROM Managers";
+            var command = new SqlCommand("sp_getAllManager", conn);
+            conn.Open ();
+            command.CommandType = CommandType.StoredProcedure;
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    manager.Add(new Managers
+                    {
+                        mId = Convert.ToInt32(reader["userId"]),
+                        mfirstName = reader["firstName"].ToString(),
+                        mlastName = reader["lastName"].ToString(),
+                        email = reader["email"].ToString(),
+                        pass = reader["pass"].ToString(),
+                        mobileNo = reader["mobileNo"].ToString()
+
+                    });
+                }
+            }
+        }
+        return manager;
+    }
+
+
+
+
+
+
+
 }
+
 
