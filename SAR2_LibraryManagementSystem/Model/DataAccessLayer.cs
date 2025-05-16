13dd0f134d7d8aa58c589708b468b5c1d3807675
charlie2307 <riteshalim237@gmail.com>
2025-05-16 15:13:39 +0530

    remove addBooks

--- SAR2_LibraryManagementSystem/Model/DataAccessLayer.cs
+++ SAR2_LibraryManagementSystem/Model/DataAccessLayer.cs
@@ -34,25 +34,6 @@ public class DataAccessLayer
     }
 
 
-    public void AddBooks(Users user)
-    {
-        using (var con = new SqlConnection(_connectionString))
-        {
-            con.Open();
-
-            //string sqlquery = "insert into Users(firstName, lastName, email, pass, mobileNo) values (@firstName, @lastName, @email, @pass, @mobileNo)";
-            using (var cmd = new SqlCommand("sp_addBooks", con))
-            {
-                cmd.CommandType = CommandType.StoredProcedure;
-                cmd.Parameters.AddWithValue("@firstName", user.firstName);
-                cmd.Parameters.AddWithValue("@lastName", user.lastName);
-                cmd.Parameters.AddWithValue("@email", user.email);
-                cmd.Parameters.AddWithValue("@pass", user.pass);
-                cmd.Parameters.AddWithValue("@mobileNo", user.mobileNo);
-
-                int affectedrow = cmd.ExecuteNonQuery();
-            }
-        }
-    }
+    
 
 }
