d8de9b769080b3e3dbf50c685578c3b3db196cb2
charlie2307 <riteshalim237@gmail.com>
2025-05-17 11:55:12 +0530

    login

--- SAR2_LibraryManagementSystem/Program.cs
+++ SAR2_LibraryManagementSystem/Program.cs
@@ -1,5 +1,8 @@
 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using SAR2_LibraryManagementSystem.Model;
+using System.Text;
 
 namespace SAR2_LibraryManagementSystem
 {
@@ -19,6 +22,23 @@ namespace SAR2_LibraryManagementSystem
             builder.Services.AddScoped<DataAccessLayer>();
             builder.Services.AddScoped<ManagerDAL>();
             builder.Services.AddScoped<BooksDAL>();
+
+            //JWT
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                        ValidAudience = builder.Configuration["Jwt:Issuer"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+
+                    };
+                });
 
             var app = builder.Build();
 
