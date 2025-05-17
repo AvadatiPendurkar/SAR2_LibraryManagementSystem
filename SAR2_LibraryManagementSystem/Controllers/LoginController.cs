b6f2538fa203199be06cf2c1730af3ecb623821f
charlie2307 <riteshalim237@gmail.com>
2025-05-17 18:17:13 +0530

    login controler

--- SAR2_LibraryManagementSystem/Controllers/LoginController.cs
+++ SAR2_LibraryManagementSystem/Controllers/LoginController.cs
@@ -1,5 +1,10 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using SAR2_LibraryManagementSystem.Model;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace SAR2_LibraryManagementSystem.Controllers
 {
@@ -7,5 +12,42 @@ namespace SAR2_LibraryManagementSystem.Controllers
     [ApiController]
     public class LoginController : ControllerBase
     {
+        public readonly IConfiguration config;
+
+        public LoginController(IConfiguration configuration)
+        {
+            config = configuration;
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public string GenToken(Login admin)
+        {
+            string token = "";
+            if (admin.email == "admin@gmail.com" && admin.password == "123")
+            {
+                var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Jwt:Key"]));
+                var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+                var claims = new[]
+                {
+                    new Claim("Email",admin.email),
+                    new Claim("role","Admin"),
+                    new Claim("role","User"),
+                    new Claim("role","Manager")
+                };
+
+                var tokenFinal = new JwtSecurityToken(config["Jwt:Issuer"],
+                    config["Jwt:Issuer"],
+                    claims,
+                    expires: DateTime.Now.AddMinutes(120),
+                    signingCredentials: credentials
+                );
+
+                token = new JwtSecurityTokenHandler().WriteToken(tokenFinal);
+
+            }
+            return token;
+        }
     }
 }
