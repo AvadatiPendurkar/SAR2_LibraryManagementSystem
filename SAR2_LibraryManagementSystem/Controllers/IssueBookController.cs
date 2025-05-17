0e7f8a670a1d8e205207b22434d05d071836e18b
Samruddhi Sarvankar <samruddhi25900@gmail.com>
2025-05-17 14:40:21 +0530

    Issuebook dal and controller

--- SAR2_LibraryManagementSystem/Controllers/IssueBookController.cs
+++ SAR2_LibraryManagementSystem/Controllers/IssueBookController.cs
@@ -21,5 +21,43 @@ namespace SAR2_LibraryManagementSystem.Controllers
             _issueBookDAL.AddIssueBooks(issueBook);
             return Ok("Book Issued Successfully");
         }
+
+        [HttpPut]
+        public IActionResult UpdateIssueBooks(IssueBook issueBook)
+        {
+            if (issueBook.issueId <= 0)
+                return BadRequest("Invalid Issued Book id.");
+
+            _issueBookDAL.UpdateIssueBooks(issueBook);
+            return Ok(new { success = true, message = "Issued book updated successfully." });
+        }
+
+        [HttpDelete("{issueId}")]
+        public IActionResult DeleteIssueBooks(int issueId)
+        {
+            if (issueId <= 0)
+                return BadRequest("Invalid issue book id");
+
+            _issueBookDAL.DeleteIssueBook(issueId);
+            return Ok(new { success = true, message = "Issue book deleted successfully" });
+        }
+
+        [HttpGet("ViewAllIssueBook")]
+        public IActionResult ViewAllIssueBook()
+        {
+            var issueBooks = _issueBookDAL.ViewIssuesBooks();
+            return Ok(issueBooks);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetIssueBookById(int id)
+        {
+            var issuebook = _issueBookDAL.ViewIssueBooksById(id);
+            if(issuebook == null)
+            {
+                return NotFound(new { Message = $"Issue book with ID {id} not found." });
+            }
+            return Ok(issuebook);
+        }
     }
 }
--- SAR2_LibraryManagementSystem/Model/IssueBookDAL.cs
+++ SAR2_LibraryManagementSystem/Model/IssueBookDAL.cs
@@ -35,5 +35,105 @@ namespace SAR2_LibraryManagementSystem.Model
 
             }
         }
+
+        public void UpdateIssueBooks(IssueBook issueBook)
+        {
+            using (var con = new SqlConnection(_connectionString))
+            {
+                con.Open();
+
+                using (var cmd = new SqlCommand("", con))
+                {
+                    cmd.Parameters.AddWithValue("@issueId", issueBook.issueId);
+                    cmd.Parameters.AddWithValue("@userId", issueBook.userId);
+                    cmd.Parameters.AddWithValue("@bookId", issueBook.bookId);
+                    //cmd.Parameters.AddWithValue("@issueDate", issueBook.issueDate);
+                    //cmd.Parameters.AddWithValue("@dueDate", issueBook.dueDate);
+                    cmd.Parameters.AddWithValue("@bookQty", issueBook.bookQty);
+                    cmd.Parameters.AddWithValue("@status", issueBook.status);
+
+                    int affectedrow = cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void DeleteIssueBook(int issueBookId)
+        {
+            using (var con = new SqlConnection(_connectionString))
+            {
+                con.Open();
+
+                using (var cmd = new SqlCommand("",con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@issueId", issueBookId);
+
+                    int affectedrow = cmd.ExecuteNonQuery();
+
+                }
+            }
+        }
+
+        public List<IssueBook> ViewIssuesBooks()
+        {
+            var IssueBooks = new List<IssueBook>();
+            using (var con = new SqlConnection(_connectionString))
+            {
+                var cmd = new SqlCommand("", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                con.Open();
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        IssueBooks.Add(new IssueBook
+                        {
+                            issueId = Convert.ToInt32(reader["issueId"]),
+                            userId = Convert.ToInt32(reader["userId"]),
+                            bookId = Convert.ToInt32(reader["bookId"]),
+                            issueDate = Convert.ToDateTime(reader["issueDate"]),
+                            dueDate = Convert.ToDateTime(reader["dueDate"]),
+                            bookQty = Convert.ToInt32(reader["bookQty"]),
+                            status = reader["status"].ToString()
+                        });
+                    }
+                }
+            }
+            return IssueBooks;
+        }
+
+        public IssueBook ViewIssueBooksById(int issueId)
+        {
+            IssueBook issueBook = null;
+            using (var con = new SqlConnection(_connectionString))
+            {
+                con.Open();
+
+                using (var cmd = new SqlCommand("", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@issueId", issueId);
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            issueBook = new IssueBook
+                            {
+                                issueId = Convert.ToInt32(reader["issueId"]),
+                                userId = Convert.ToInt32(reader["userId"]),
+                                bookId = Convert.ToInt32(reader["bookId"]),
+                                issueDate = Convert.ToDateTime(reader["issueDate"]),
+                                dueDate = Convert.ToDateTime(reader["dueDate"]),
+                                bookQty = Convert.ToInt32(reader["bookQty"]),
+                                status = reader["status"].ToString()
+                            };
+                        }
+                    }
+                }
+                return issueBook;
+            }
+        }
     }
 }
