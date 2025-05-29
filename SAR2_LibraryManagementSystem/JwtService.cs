0ebf8fcd3f911d4146b06e3f249a18ebce9377e9
charlie2307 <riteshalim237@gmail.com>
2025-05-29 14:47:29 +0530

    jwtService

---
+++ SAR2_LibraryManagementSystem/JwtService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using SAR2_LibraryManagementSystem.Model;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace SAR2_LibraryManagementSystem
+{
+    public class JwtService
+    {
+        private readonly string key = string.Empty;
+        private readonly int duration;
+
+        public JwtService(IConfiguration configuration)
+        {
+            key = configuration["Jwt:Key"]!;
+            duration = int.Parse(configuration["Jwt:Duration"]!);
+        }
+
+        public string GenerateToken(Users user)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(this.key));
+            var signingKey = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim("userId", user.userId.ToString()),
+                new Claim("firstName", user.firstName),
+                new Claim("lastName", user.lastName),
+                new Claim("email", user.email),
+                new Claim("mobileNo", user.mobileNo),
+                new Claim("pass", user.pass)
+            };
+
+            var jwtToken = new JwtSecurityToken(
+                issuer: "localhost",
+                audience: "localhost",
+                claims: claims,
+                notBefore: DateTime.Now,
+                expires: DateTime.Now.AddMinutes(this.duration),
+                signingKey);
+
+            return new JwtSecurityTokenHandler().WriteToken(jwtToken);
+        }
+    }
+}
